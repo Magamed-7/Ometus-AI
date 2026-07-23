38bd3519a0f52b34c6f60a5c9b4917bb7ad78e08
Magamed-7 <teachermaga7@gmail.com>
2026-07-23 18:48:13 +0500

    add en locale

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./lib/auth/AuthContext.jsx";
+import { I18nProvider } from "./lib/i18n.jsx";
 import { ThemeProvider } from "./lib/theme.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
+      <I18nProvider>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </I18nProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
