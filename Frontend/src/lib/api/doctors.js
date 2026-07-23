f8d47ec2a5afce4b9b44d8f5150cc514ded24cb6
Magamed-7 <teachermaga7@gmail.com>
2026-07-23 19:19:36 +0500

    add doctors api module

---
+++ Frontend/src/lib/api/doctors.js
@@ -0,0 +1,16 @@
+import { client } from "./client.js";
+
+export function searchDoctors({ specialization, department_id, filial_id } = {}) {
+  const params = new URLSearchParams();
+  if (specialization) params.set("specialization", specialization);
+  if (department_id) params.set("department_id", department_id);
+  if (filial_id) params.set("filial_id", filial_id);
+  const qs = params.toString();
+
+  return client.get(`/api/doctors${qs ? `?${qs}` : ""}`, { auth: false });
+}
+
+export const getDoctor = (id) => client.get(`/api/doctors/${id}`, { auth: false });
+
+export const getDoctorDepartments = (id) =>
+  client.get(`/api/doctors/${id}/departments`, { auth: false });
