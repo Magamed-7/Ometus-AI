116fcea9dabd6a5e048058a6d05c41c6d660723d
Magamed-7 <teachermaga7@gmail.com>
2026-07-23 18:27:17 +0500

    add tailwind and light theme tokens

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>Ometus Clinic</div>
+    <div className="p-md text-primary">Ometus Clinic</div>
   </React.StrictMode>
 );
