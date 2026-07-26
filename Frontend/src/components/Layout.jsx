697f2763a4a8998fad0ea6b1384c434651407cf5
Magamed-7 <teachermaga7@gmail.com>
2026-07-26 19:06:36 +0500

    add page transitions

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { useT } from "../lib/i18n.jsx";
 import BottomNav from "./BottomNav.jsx";
 import Footer from "./Footer.jsx";
@@ -6,6 +7,11 @@ import TopNav from "./TopNav.jsx";
 
 export default function Layout() {
   const t = useT();
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  }, [location.pathname]);
 
   return (
     <div className="relative flex min-h-screen flex-col">
@@ -14,7 +20,9 @@ export default function Layout() {
       </a>
       <TopNav />
       <main id="main" tabIndex={-1} className="flex-1 pb-24 md:pb-0">
-        <Outlet />
+        <div key={location.pathname} className="page-enter">
+          <Outlet />
+        </div>
       </main>
       <Footer />
       <BottomNav />
